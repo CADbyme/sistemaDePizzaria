c91b208982ad2c91d471ffaca2129aec85d67da8
Carlos Eduardo Cavalcante <carloseduardo220905@gmail.com>
2025-08-06 14:45:09 -0300

    Add files via upload

--- Pizzaria/Pizzaria.java
+++ Pizzaria/Pizzaria.java
@@ -1,5 +1,11 @@
 public class Pizzaria {
-
+    private short idDaFilial;
     private boolean aberto;
+    private String endereco;
 
+    public void Pizzaria(short idDaFilial, boolean aberto, String endereco){
+        this.idDaFilial = idDaFilial;
+        this.aberto = aberto;
+        this.endereco = endereco;
+    };
 }
